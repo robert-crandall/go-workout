9217e791496d453d23529c349f323fe47dfe7a61
Robert Crandall <robert-crandall@github.com>
2023-01-25 08:46:25 -0800

    Add alternate lifts

--- programs/helpers.go
+++ programs/helpers.go
@@ -1,6 +1,14 @@
 package programs
 
+import "main/lifts"
+
 // Return a lift from the given lifts based on week number
 func alertnateWorkouts(workouts []workout, weeknum int) workout {
 	return workouts[weeknum%len(workouts)]
+}
+
+// Rotate through the lifts based on the day and number
+func alternateLifts(lifts []lifts.Lift, weeknum, daynum, daysPerWeek int) lifts.Lift {
+	absoluteDay := weeknum*daysPerWeek + daynum
+	return lifts[absoluteDay%len(lifts)]
 }
