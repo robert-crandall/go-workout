f01c8e0c780135fc4f263cbcaf2e3cfa4dc7371b
Robert Crandall <robert-crandall@github.com>
2023-01-25 15:31:08 -0800

    Add lift schemes

--- sets/lift_schemes.go
+++ sets/lift_schemes.go
@@ -1,5 +1,10 @@
 package sets
 
+import (
+	"fmt"
+	"os"
+)
+
 type LiftScheme int
 
 const (
@@ -8,6 +13,7 @@ const (
 	ThreeByEight
 	RptFourSets
 	RptFiveSets
+	OneRepMaxTest
 )
 
 func (s LiftScheme) String() string {
@@ -19,9 +25,53 @@ func (s LiftScheme) String() string {
 	case ThreeByEight:
 		return "3x8"
 	case RptFourSets:
-		return "RPT 4 sets"
+		return "RPT"
 	case RptFiveSets:
-		return "RPT 5 sets"
+		return "RPT"
+	case OneRepMaxTest:
+		return "1RM Test"
 	}
 	return "unknown"
+}
+
+func (s LiftScheme) IsRPT() bool {
+	switch s {
+	case RptFourSets, RptFiveSets:
+		return true
+	}
+	return false
+}
+
+func (s LiftScheme) Is1RM() bool {
+	switch s {
+	case OneRepMaxTest:
+		return true
+	}
+	return false
+}
+
+func (s LiftScheme) Sets() int {
+	switch s {
+	case FiveByFive, RptFiveSets:
+		return 5
+	case ThreeByFive, ThreeByEight, OneRepMaxTest:
+		return 3
+	case RptFourSets:
+		return 4
+	}
+	fmt.Printf("%s - Lift Scheme does not have a Set Count\n", s.String())
+	os.Exit(1)
+	return 0
+}
+
+func (s LiftScheme) Reps() int {
+	switch s {
+	case FiveByFive, ThreeByFive, OneRepMaxTest:
+		return 5
+	case ThreeByEight:
+		return 8
+	}
+	fmt.Printf("%s - Lift Scheme does not have a Rep Count\n", s.String())
+	os.Exit(1)
+	return 0
 }
